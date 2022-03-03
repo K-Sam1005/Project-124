1b36d0ee9f06309533026e71d5d9ce3cc8184afc
K Sam <81320998+K-Sam1005@users.noreply.github.com>
2022-03-03 14:12:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(600,450);
+    canvas.position(560,100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized...");
+}
+
+function draw(){
+    background('#5196e3');
+}
